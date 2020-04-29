8940c7ebd9b70b238f476d4acd8d5f026a0ea033
igor <igor.pro.dev1@gmail.com>
2020-04-29 20:51:59 +0300

    fix

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -274,22 +274,27 @@ export class MainComponent implements OnInit, AfterViewInit {
     this.drowLines();
 
     this.data.forEach((element, index, arr) => {
+      let d, dx, dy, color;
+      let count = 0;
+      let countS = 0;
+      
+      let h = (65 + (count > 3 ? ((count - 3) * 27 + ( countS * 5) + (count * 5)) : 0+ ( countS * 5)));
+      let selected = (this.selected !== null && (+this.selected === +index)) ? "stroke-width:1;stroke:rgb(0,0,0)" : "";
+      let g = this.conteiner.append("g").attr("class", "g");
       switch (element.objectClass) {
         case "Data Model":
         case "Audit Rules":
         case "Contextual Rules":
         case "Role type x":
         // case "Board":
-          let d, dx, dy, color;
           dx = element.x - 10;
           dy = element.y - 8;
           color = this.colors[element.objectClass];
-          let count = 0;
-          let countS = 0;
-
-          let h = (65 + (count > 3 ? ((count - 3) * 27 + ( countS * 5) + (count * 5)) : 0+ ( countS * 5)));
-          let selected = (this.selected !== null && (+this.selected === +index)) ? "stroke-width:1;stroke:rgb(0,0,0)" : "";
-          let g = this.conteiner.append("g").attr("class", "g");
+          count = 0;
+          countS = 0;
+          h = (65 + (count > 3 ? ((count - 3) * 27 + ( countS * 5) + (count * 5)) : 0+ ( countS * 5)));
+          selected = (this.selected !== null && (+this.selected === +index)) ? "stroke-width:1;stroke:rgb(0,0,0)" : "";
+          g = this.conteiner.append("g").attr("class", "g");
           g.append("rect")
             .attr("class", "nodes")
             .attr("id", index)
@@ -297,8 +302,8 @@ export class MainComponent implements OnInit, AfterViewInit {
             .attr("fill", color)
             .attr("x", element.x - 5)
             .attr("y", element.y - 10)
-            .attr("width", 160)
-            .attr("height", h)
+            .attr("width", 140)
+            .attr("height", 35)
             .attr("rx", 10)
             .attr("ry", 10)
             .call(
@@ -347,7 +352,15 @@ export class MainComponent implements OnInit, AfterViewInit {
               this.activeArrow = null;
               this.startDrowLine = null;
             });
-
+          g.append("svg")
+            .attr("x", element.x + 5)
+            .attr("y", element.y - 5)
+            .attr("width", 24)
+            .attr("height", 24)
+            .attr("viewBox", "0 0 24 24")
+            .append("path")
+            .attr("d", "M24 13.616v-3.232c-1.651-.587-2.694-.752-3.219-2.019v-.001c-.527-1.271.1-2.134.847-3.707l-2.285-2.285c-1.561.742-2.433 1.375-3.707.847h-.001c-1.269-.526-1.435-1.576-2.019-3.219h-3.232c-.582 1.635-.749 2.692-2.019 3.219h-.001c-1.271.528-2.132-.098-3.707-.847l-2.285 2.285c.745 1.568 1.375 2.434.847 3.707-.527 1.271-1.584 1.438-3.219 2.02v3.232c1.632.58 2.692.749 3.219 2.019.53 1.282-.114 2.166-.847 3.707l2.285 2.286c1.562-.743 2.434-1.375 3.707-.847h.001c1.27.526 1.436 1.579 2.019 3.219h3.232c.582-1.636.75-2.69 2.027-3.222h.001c1.262-.524 2.12.101 3.698.851l2.285-2.286c-.744-1.563-1.375-2.433-.848-3.706.527-1.271 1.588-1.44 3.221-2.021zm-12 2.384c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4z")
+            // .text((element.name || element.id));
           // g.append("text")
           //   .attr("x", element.x - 5)
           //   .attr("y", element.y - 13)
@@ -432,6 +445,95 @@ export class MainComponent implements OnInit, AfterViewInit {
 
           break;
 
+          case "AND":
+          case "OR":
+            dx = element.x - 10;
+            dy = element.y - 8;
+            color = this.colors[element.objectClass];
+            count = 0;
+            countS = 0;
+            h = (65 + (count > 3 ? ((count - 3) * 27 + ( countS * 5) + (count * 5)) : 0+ ( countS * 5)));
+            selected = (this.selected !== null && (+this.selected === +index)) ? "stroke-width:1;stroke:rgb(0,0,0)" : "";
+            g = this.conteiner.append("g").attr("class", "g");
+    
+              g.append("rect")
+                .attr("class", "nodes")
+                .attr("id", index)
+                .attr("style", selected)
+                .attr("fill", color)
+                .attr("x", element.x + 90)
+                .attr("y", element.y - 10)
+                .attr("width", 50)
+                .attr("height", 50)
+                .attr("rx", 15)
+                .attr("ry", 15)
+   
+                .call(
+                  d3
+                    .drag()
+                    .on("start", dragstarted)
+                    .on("drag", dragged)
+                    .on("end", dragended)
+                )
+                .on("mouseover", (q, w, e) => {
+                  d3.event.stopPropagation();
+                  if (this.activeArrow) {
+                    document.documentElement.style.cursor = "default";
+                    d3.select(document.getElementById(e[0].id + "main")).style(
+                      "fill",
+                      "#84bd96"
+                    );
+                  }
+                })
+                .on("mouseout", (q, w, e) => {
+                  d3.event.stopPropagation();
+                  d3.select(document.getElementById(e[0].id + "main")).style(
+                    "fill",
+                    "#2196f3"
+                  );
+                  if (this.activeArrow) {
+                    document.documentElement.style.cursor = "not-allowed";
+                  }
+                })
+                .on("click", (d, i, s) => {
+                  d3.event.stopPropagation();
+                  this.selectedModal = s[0].id;
+                  this.selected = s[0].id;
+                  this.removeAll();
+                  this.drow();
+                  if (this.activeArrow)
+                    this.shepClick(s[0].id);
+                })
+                .on("dblclick", (d, i, s) => {
+                  this.selectedModal = s[0].id;
+                  let name = this.data[this.selectedModal].objectClass + (this.data[this.selectedModal].parameters.length + 1);
+                  this.showSide = true;
+                  this.selected = s[0].id;
+                  this.removeAll();
+                  this.drow();
+                  this.activeArrow = null;
+                  this.startDrowLine = null;
+                });
+    
+              g.append("text")
+                .attr("id", index + "-remove")
+                .attr("x", element.x + 140)
+                .attr("y", element.y - 13)
+                .text("X")
+                .attr("cursor", "pointer")
+                .on("click", (d, i, s) => {
+                  d3.event.stopPropagation();
+                  let id = s[0].id.split("-")[0];
+    
+                });
+
+          g.append("text")
+            .attr("x", element.x+105 + (element.objectClass === "AND" ? -8 : 0))
+            .attr("y", element.y + 20)
+            .attr("fill", "white")
+            .text(element.objectClass);
+    
+              break;
         default:
           break;
       }
@@ -473,7 +575,7 @@ export class MainComponent implements OnInit, AfterViewInit {
 
         self.dragSelected = this.getAttribute("id").split("-")[0];
         self.data[self.dragSelected].x =
-          (d3.event.x - self.zoomTrans.x) / self.zoomTrans.k;
+          (d3.event.x - self.zoomTrans.x) / self.zoomTrans.k - 90;
         // self.start_x + (d3.event.x - self.start_x) / current_scale;
         // (e.offsetX - this.zoomTrans.x) / this.zoomTrans.k;
         let scale = 30;
@@ -481,7 +583,7 @@ export class MainComponent implements OnInit, AfterViewInit {
           scale = 50;
         }
         self.data[self.dragSelected].y = (
-          (d3.event.y - self.zoomTrans.y) / self.zoomTrans.k - (scale / self.zoomTrans.k)) - 25;
+          (d3.event.y - self.zoomTrans.y) / self.zoomTrans.k - (scale / self.zoomTrans.k)) + 5;
         // self.start_y + (d3.event.y - self.start_y) / current_scale;
         self.removeAll();
         self.drow();
@@ -540,12 +642,12 @@ export class MainComponent implements OnInit, AfterViewInit {
 
           var d = {
             source: {
-              x: x + 30,
-              y: y + 15
+              x: x + 110,
+              y: y + 10
             },
             target: {
-              x: x2 + 30,
-              y: y2 + 15
+              x: x2 + 110,
+              y: y2 + 10
             }
           };
 
@@ -645,6 +747,24 @@ export class MainComponent implements OnInit, AfterViewInit {
       }
     });
 
+    document.getElementById("AND").addEventListener(
+      "dragstart",
+      ev => {
+        ev.dataTransfer.setData('text', 'foo');
+        this.dragType = "AND";
+      },
+      false
+    );
+
+    document.getElementById("OR").addEventListener(
+      "dragstart",
+      ev => {
+        ev.dataTransfer.setData('text', 'foo');
+        this.dragType = "OR";
+      
+      },
+      false
+    );
     document.addEventListener("dragover", function (event) {
       event.preventDefault();
     });
@@ -665,7 +785,7 @@ export class MainComponent implements OnInit, AfterViewInit {
         console.log(this.data);
         
         let model = new ComponentClass();
-        model.x = x;
+        model.x = x - 100;
         model.y = y;
         model.objectClass = this.dragType;
         model.id = this.uuidv4();
